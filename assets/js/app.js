2f99c4acc8c9335b766b7f03067f8e7a36324bc0
Ximo Romero <ximore55@gmail.com>
2025-03-31 09:05:19 +0200

    finish challange

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,177 @@
 document.addEventListener("DOMContentLoaded", async () => {
   console.log("Aplicación iniciada 🚀");
+
+  const mortgageForm = document.getElementById("mortgageForm");
+  const clearAllButton = document.getElementById("clearAllButton");
+  const resultsContainer = document.querySelector(
+    ".mortgage-calculator__results"
+  );
+
+  //Reset form button, removes the focus from the radiobuttons as well
+  clearAllButton.addEventListener("click", () => {
+    document.querySelectorAll(".mortgage-calculator__radio").forEach((l) => {
+      l.classList.remove("active");
+    });
+    mortgageForm.reset();
+  });
+
+  //Add active effect to the radiobuttons if selected
+  document.querySelectorAll(".mortgage-calculator__radio").forEach((label) => {
+    const input = label.querySelector("input");
+    input.addEventListener("change", () => {
+      document.querySelectorAll(".mortgage-calculator__radio").forEach((l) => {
+        l.classList.remove("active");
+      });
+      if (input.checked) {
+        label.classList.add("active");
+      }
+    });
+  });
+
+  //Add decimal to amount input
+  const amountInput = document.getElementById("amount");
+
+  amountInput.addEventListener("input", (e) => {
+    let value = e.target.value.replace(/\D/g, "");
+    value = new Intl.NumberFormat("de-DE").format(value);
+
+    e.target.value = value;
+  });
+
+  // Function to handle error message creation and removal
+  const handleErrorMessage = (container, message) => {
+    let errorMessage = container.querySelector(".errorMessage");
+    if (!errorMessage) {
+      errorMessage = document.createElement("p");
+      errorMessage.innerText = message;
+      errorMessage.className = "errorMessage";
+      container.append(errorMessage);
+    }
+  };
+
+  // Function to remove error message
+  const removeErrorMessage = (container) => {
+    const errorMessage = container.querySelector(".errorMessage");
+    if (errorMessage) {
+      errorMessage.remove();
+    }
+  };
+
+  //Form validation
+  mortgageForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let isValid = true;
+
+    //Input number validation
+    const fields = mortgageForm.querySelectorAll(
+      ".mortgage-calculator__field:not(.mortgage-calculator__field--inline), .mortgage-calculator__subfield"
+    );
+
+    fields.forEach((field) => {
+      const input = field.querySelector("input[type=number]");
+
+      if (!input) return;
+
+      const inputContainer = field.querySelector(
+        ".mortgage-calculator__input-container"
+      );
+
+      if (!inputContainer) {
+        console.error(
+          "No se encontró .mortgage-calculator__input-container en",
+          field
+        );
+        return;
+      }
+
+      if (input.value.trim() === "") {
+        inputContainer.classList.add("error");
+        const icon = inputContainer.querySelector(".icon");
+        if (icon) icon.classList.add("error");
+
+        // Add the error message to the field container
+        handleErrorMessage(field, "This field is required");
+
+        isValid = false;
+      } else {
+        inputContainer.classList.remove("error");
+        const icon = inputContainer.querySelector(".icon");
+        if (icon) icon.classList.remove("error");
+
+        // Remove the error message if the field is filled
+        removeErrorMessage(field);
+      }
+    });
+
+    //Input radio validation
+    const radios = document.querySelectorAll("input[name='type']");
+    const isChecked = Array.from(radios).some((radio) => radio.checked);
+
+    const radioContainer = radios[0].closest(".mortgage-calculator__field");
+    if (!isChecked) {
+      handleErrorMessage(radioContainer, "This field is required");
+
+      isValid = false;
+    } else {
+      removeErrorMessage(radioContainer);
+    }
+
+    if (!isValid) return;
+
+    let amount = parseFloat(amountInput.value.replace(/\./g, ""));
+    let term = parseInt(document.getElementById("term").value);
+    let interestRate = parseFloat(document.getElementById("interest").value);
+    let mortgageType = document.querySelector(
+      "input[name='type']:checked"
+    ).value;
+
+    if (isNaN(amount) || isNaN(term) || isNaN(interestRate)) {
+      alert("Please enter valid numbers");
+      return;
+    }
+
+    // Calcular número total de pagos (meses)
+    let n = term * 12;
+
+    // Calcular tasa de interés mensual
+    let r = interestRate / 100 / 12;
+
+    let monthlyPayment = 0;
+    let totalRepayment = 0;
+
+    if (mortgageType === "repayment") {
+      // Fórmula de amortización
+      monthlyPayment =
+        (amount * (r * Math.pow(1 + r, n))) / (Math.pow(1 + r, n) - 1);
+      totalRepayment = monthlyPayment * n;
+    } else {
+      // "Interest Only" mortgage (solo paga intereses mensuales)
+      monthlyPayment = amount * r;
+      totalRepayment = monthlyPayment * n;
+    }
+
+    // Formatear los resultados en euros (€)
+    let formatter = new Intl.NumberFormat("es-ES", {
+      style: "currency",
+      currency: "EUR",
+      minimumFractionDigits: 2,
+    });
+
+    resultsContainer.innerHTML = `
+    <h3 class="results__title">Your results</h3>
+    <p class="results__desc">Your results are shown below based on the information you provided. To adjust the results, edit the form and click "Calculate repayments" again.</p>
+      <div class="results-content">
+        <div>
+          <p>Your monthly repayments</p>
+          <h2>${formatter.format(monthlyPayment)}</h2>
+        </div>
+        <hr/>
+        <div>
+          <p>Total you'll repay over the term</p>
+          <h3>${formatter.format(totalRepayment)}</h3>
+        </div>
+      </div>
+    `;
+  });
 });
---
+++ readme.md
@@ -0,0 +1,53 @@
+# Frontend Mentor - Mortgage repayment calculato solution
+
+This is a solution to the [Mortgage repayment calculator](https://www.frontendmentor.io/challenges/mortgage-repayment-calculator-Galx1LXK73).
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+- [Extra](#extra)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Input mortgage information and see monthly repayment and total repayment amounts after submitting the form
+- See form validation messages if any field is incomplete
+- Complete the form only using their keyboard
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+### Screenshot
+
+![](./screenshot.png)
+
+### Links
+
+- Solution URL: [Solution here](https://github.com/Ximoromero6/mortgage-challange)
+- Live Site URL: [Live site URL here](https://ximoromero6.github.io/mortgage-challange/)
+
+## My process
+
+### Built with
+
+- HTML5 markup
+- SCSS
+- Vanilla Javascript
+
+## Author
+
+- Website - [ximoromero.es](https://www.ximoromero.es)
+- Frontend Mentor - [@Ximoromero6](https://www.frontendmentor.io/ximoromero6)
+- Instagram - [@Ximoromero5](https://www.instagram.com/Ximoromero5)
+
+## Extra
+
+I have added the functionality to save the status of the modifications through Localstorage, so that the changes will be saved even if the browser is refreshed.
